ba247b28e8de0bd44db4dd8403a1ea06b1dc4eb3
zakno52 <101570337+zakno52@users.noreply.github.com>
2025-05-30 22:02:31 +0100

    https

--- src/API/Fetch.js
+++ src/API/Fetch.js
@@ -4,7 +4,7 @@ export class FetchAndReturnData {
     // eslint-disable-next-line no-useless-catch
     try {
       const response = await fetch(
-        `http://api.weatherapi.com/v1/forecast.json?key=f1f9fb8473e64e31b6a135518252205&q=${location}&days=7&aqi=no&alerts=no`
+        `https://api.weatherapi.com/v1/forecast.json?key=f1f9fb8473e64e31b6a135518252205&q=${location}&days=7&aqi=no&alerts=no`
       );
       const data = await response.json();
 
